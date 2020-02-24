de43266674e098fe22056d06d21bc45d351730e3
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-02-24 16:27:29 +0000

    Completed step 6

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,11 +1,13 @@
 class OysterCard
-    attr_accessor :balance
-    DEFAULT_BALANCE = 0
+	attr_accessor :balance
+	DEFAULT_BALANCE = 0
+	MAXIMUM_BALANCE = 90
+	def initialize(balance = DEFAULT_BALANCE)
+		@balance = balance
+	end
 
-    def initialize(balance = DEFAULT_BALANCE)
-        @balance = balance
-    end
-    def top_up(num)
-      return @balance += num
-    end
+	def top_up(num)
+		fail 'You cant top up more than £90 balance' if (@balance + num) > MAXIMUM_BALANCE
+		@balance += num
+	end
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -5,12 +5,17 @@ describe OysterCard do
 # In order to use public transport
 # As a customer
 # I want money on my card
-    it 'shows me my default balance' do
-        expect(subject.balance).to eq OysterCard::DEFAULT_BALANCE
-    end
+  it 'shows me my default balance' do
+      expect(subject.balance).to eq OysterCard::DEFAULT_BALANCE
+  end
 
-    it 'top up the oyster card balance' do
-      num = 9
-      expect(subject.top_up(num)).to eq num
-    end
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  it 'can top up the balance' do
+    expect{ subject.top_up 1}.to change{ subject.balance }.by 1
+  end
+
+  it 'will fail if you try and top up past the maximum balance allowed, £90' do
+    expect{subject.top_up(91)}.to raise_error 'You cant top up more than £90 balance'
+  end
 end
