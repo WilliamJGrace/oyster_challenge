30a74f73e97493ae2d082f089b7395134097d175
Patrick Oliver <poliver24@hotmail.com>
2020-02-26 15:12:34 +0000

    ability to complete and record a journey to our journeys variable when touching out has been added; tests and code have been refactored to adhere to DRY; step 12 - complete

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,12 +1,12 @@
 class OysterCard
-	attr_reader :balance , :entry_station
+	attr_reader :balance , :entry_station, :journeys
 	DEFAULT_BALANCE = 0
 	MAXIMUM_BALANCE = 90
 	MINIMUM_AMOUNT = 1
 	def initialize(balance = DEFAULT_BALANCE)
 		@balance = balance
 		@entry_station
-
+		@journeys = []
 	end
 
 	def top_up(num)
@@ -15,17 +15,18 @@ class OysterCard
 	end
 
 
-	def touch_in(station)
+	def touch_in(entry_station)
 		fail "You cannot travel as you have less than £#{MINIMUM_AMOUNT}" if @balance < MINIMUM_AMOUNT
-		@entry_station = station
+		@entry_station = entry_station
 	end
 
 	def in_journey?
     !!@entry_station
 	end
 
-	def touch_out
+	def touch_out(exit_station)
 		deduct
+		@journeys << {entry_station: @entry_station, exit_station: exit_station}
 		@entry_station = nil
 	end
 
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -6,9 +6,10 @@ describe OysterCard do
   # basic card instance with default starting balance
   let(:card) {OysterCard.new}
   #station double
-  let(:station){ double :station }
-
-
+  let(:entry_station){ double :entry_station }
+  let(:exit_station){ double :exit_station }
+  #journey double
+  let(:journey){ {entry_station: entry_station, exit_station: exit_station} }
 
   it 'shows me my default balance' do
       expect(subject.balance).to eq OysterCard::DEFAULT_BALANCE
@@ -30,12 +31,12 @@ describe OysterCard do
     # it { is_expected.to respond_to{:touch_in}
 
     it 'start the journey' do
-      card_min_bal.touch_in(station)
+      card_min_bal.touch_in(entry_station)
       expect(card_min_bal).to be_in_journey
     end
 
     it "if mimimum amount is less than £1 you cannot travel" do
-      expect{ card.touch_in(station) }.to raise_error "You cannot travel as you have less than £#{OysterCard::MINIMUM_AMOUNT}"
+      expect{ card.touch_in(entry_station) }.to raise_error "You cannot travel as you have less than £#{OysterCard::MINIMUM_AMOUNT}"
     end
 
 
@@ -49,27 +50,35 @@ describe OysterCard do
   # This tests are for testing touch_out method.
 
   describe "#touch_out" do
-    it { is_expected.to respond_to(:touch_out)}
+      it { is_expected.to respond_to(:touch_out).with(1).argument }
 
-    it 'will change in_journey to false when user touches out' do
-      card_min_bal.touch_in(station)
-      card_min_bal.touch_out
-      expect(card_min_bal).not_to be_in_journey
-    end
+
 
     it 'will deduct the amount from the oyster card' do
-      expect{ card.touch_out }.to change{card.balance }.by -OysterCard::MINIMUM_AMOUNT
+      expect{ card.touch_out(entry_station) }.to change{card.balance }.by -OysterCard::MINIMUM_AMOUNT
     end
   end
   # testing stations
   it 'stores the entry station' do
-    card_min_bal.touch_in(station)
-    expect(card_min_bal.entry_station).to eq station
-  end
-  it 'returns station value to nil' do
-    card_min_bal.touch_in(station)
-    card_min_bal.touch_out
-    expect(card_min_bal.entry_station).to eq nil
+    card_min_bal.touch_in(entry_station)
+    expect(card_min_bal.entry_station).to eq entry_station
   end
 
+  describe 'touch_in touch_out' do
+    before(:each) do
+      card_min_bal.touch_in(entry_station)
+      card_min_bal.touch_out(exit_station)
+    end
+    it 'will change in_journey to false when user touches out' do
+      expect(card_min_bal).not_to be_in_journey
+    end
+    it 'returns station value to nil' do
+      expect(card_min_bal.entry_station).to eq nil
+    end
+    it 'records previous journeys' do
+      expect(card_min_bal.journeys).to include journey
+    end
+  end
+
+  it { expect(subject.journeys).to eq [] }
 end
